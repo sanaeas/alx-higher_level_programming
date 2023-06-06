92189d7d5cda54627c653e03d7160a659a75a5f4
Sana Assim <sanaassim1@gmail.com>
2023-06-06 05:34:21 -0700

    Fix: use one print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    if char % 2 == 0:
-        print("{:c}".format(char), end="")
-    else:
-        print("{:c}".format(char - 32), end="")
+    if char % 2 != 0:
+        char = char - 32
+    print("{:c}".format(char), end="")
