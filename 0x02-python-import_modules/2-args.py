bfc233a543d2b3d95f9b4ffa183250dd96e0099e
Sana Assim <sanaassim1@gmail.com>
2023-06-08 02:49:15 -0700

    Print the number and the list of command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
+    print(f"{len(argv) - 1} argument", end="")
     if len(argv) == 1:
-        print("0 arguments.")
+        print("s.")
+    elif len(argv) == 2:
+        print(":")
     else:
-        print(f"{len(argv) - 1} argument:")
-        for i in range(1, len(argv)):
-            print(f"{i}: {argv[i]}")
+        print("s:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
