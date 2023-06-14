00f0b4ec64d3cbc007ce7df8ae04a0f53cf41b44
Sana Assim <sanaassim1@gmail.com>
2023-06-14 05:04:01 -0700

    Find a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary:
+        best = max(list(a_dictionary.values()))
+        for key, value in a_dictionary.items():
+            if value == best:
+                return key
+    else:
         return None
-    best = max(list(a_dictionary.values()))
-    for key, value in a_dictionary.items():
-        if value == best:
-            return key
