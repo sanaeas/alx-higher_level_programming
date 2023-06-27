8c9eccc5cc47711567059fd763571064dcc0ff70
Sana Assim <sanaassim1@gmail.com>
2023-06-27 03:01:52 -0700

    Define a square by size attribute, check that size is a positive int

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module creating a Square class"""
+
+
+class Square:
+    """Define a Square class"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
