bd4fbf1adf77eef662c04682e0bc36ba270a97b7
Sana Assim <sanaassim1@gmail.com>
2023-07-02 03:20:18 -0700

    Print a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add newlines to a text"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after: ., ? and :"""
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] in [':', '.', '?']:
+            print(text[i])
+            print("")
+            i += 1
+        else:
+            print(text[i], end="")
+        i += 1
