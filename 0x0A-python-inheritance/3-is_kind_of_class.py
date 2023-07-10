db627efe336e7073034363d1c0dd33853e3188d2
Sana Assim <sanaassim1@gmail.com>
2023-07-10 02:56:16 -0700

    Check if the object is an instance of the specified class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module defines is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if the object is an instance of a class"""
+    return isinstance(obj, a_class)
