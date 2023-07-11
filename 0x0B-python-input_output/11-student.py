97b365b30debff4f6067d05ff99dfe368e2c012f
Sana Assim <sanaassim1@gmail.com>
2023-07-11 03:27:15 -0700

    Add reload_from_json() to Student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module defines a class Student"""
+
+
+class Student():
+    """a class that represents a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+        result = {}
+        for attr in attrs:
+            if type(attr) is str:
+                try:
+                    result[attr] = vars(self)[attr]
+                except Exception:
+                    pass
+        return result
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        self.__dict__ = json
