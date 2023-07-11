0207c01623dde2acaf6c7612b2f1204a9a26477e
Sana Assim <sanaassim1@gmail.com>
2023-07-11 02:56:35 -0700

    Return the dictionary description of an object

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defines class_to_json function"""
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    for JSON serialization of an object
+    """
+    return vars(obj)
