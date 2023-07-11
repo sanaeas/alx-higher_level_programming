4ab140b4469970039fd381b64287bce9c31b039c
Sana Assim <sanaassim1@gmail.com>
2023-07-11 11:20:33 -0700

    Fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """A script that reads stdin line by line and computes metrics"""
-from sys import stdin
+import sys
 
 
 def print_statistics(file_size, status_codes):
-    """prints statistics"""
+    """Print statistics"""
     print("File size: {}".format(file_size))
-    for key in status_codes.keys():
+    for key in status_code.keys():
         if status_codes[key] != 0:
             print("{}: {}".format(key, status_codes[key]))
 
 
 def main():
     """Reads stdin and computes metrics"""
-
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                     '403': 0, '404': 0, '405': 0, '500': 0}
     file_size = 0
     num_lines = 0
-
     try:
         for line in stdin:
-            splited_line = line.rstrip().split("HTTP/1.1\" ")
-            scode_fsize = splited_line[-1].split(" ")
+            splitted_line = line.rstrip().split("HTTP/1.1\" ")
+            scode_fsize = splitted_line[-1].split(" ")
 
-            status_codes[scode_fsize[0]] += 1
-            file_size += int(scode_fsize[-1])
+            status_code[scode_fsize[0]] += 1
+            size += int(scode_fsize[-1])
 
             num_lines += 1
             if num_lines >= 10:
