cd8e2cc566e697c3b58ce024ab9839a1644f79db
Sana Assim <sanaassim1@gmail.com>
2023-07-11 03:29:57 -0700

    Add reload_from_json() to Student class

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,4 +23,5 @@ class Student():
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
-        self.__dict__ = json
+        for key, val in json.items():
+            setattr(self, key, val)
