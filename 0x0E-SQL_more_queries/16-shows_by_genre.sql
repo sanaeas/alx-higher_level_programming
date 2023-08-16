547287e2731cd2097eff35e10b5c59ff1fff4c5a
Sana Assim <sanaassim1@gmail.com>
2023-08-16 04:53:10 -0700

    List all shows, and all genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all shows, and all genres linked to that show
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY title, name;
