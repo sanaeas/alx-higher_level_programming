ffacf3e75c9709eca1fd2e939d3c472fa3c3d913
Sana Assim <sanaassim1@gmail.com>
2023-08-16 08:02:41 -0700

    List all shows ordered by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows ordered by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
