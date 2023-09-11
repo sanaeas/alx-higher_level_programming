ac4de142dc98c297c5a30280a8af2ee19f9282fc
Sana Assim <sanaassim1@gmail.com>
2023-09-11 09:26:43 -0700

    Fix: convert elements to integers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const args = process.argv.slice(2).sort().reverse();
+  const args = process.argv.slice(2).map(Number).sort().reverse();
   console.log(args[1]);
 }
