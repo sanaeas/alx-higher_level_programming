34c21a1d5fee38394f0e6602c09d0c7866c1ebd9
Sana Assim <sanaassim1@gmail.com>
2023-09-11 10:06:18 -0700

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,18 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).map(Number).sort().reverse();
-  const ints = args.filter((item, index) => args.indexOf(item) === index);
-  console.log(ints[1]);
+  const args = process.argv.slice(2).map(Number);
+  let firstMax = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > firstMax) {
+      secondMax = firstMax;
+      firstMax = args[i];
+    } else if (args[i] > secondMax && args[i] !== firstMax) {
+      secondMax = args[i];
+    }
+  }
+
+  console.log(secondMax);
 }
