cee9e41864406f0cdb7298eab88bcd77908b5d4a
Sana Assim <sanaassim1@gmail.com>
2023-09-11 09:31:47 -0700

    Fix second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   const args = process.argv.slice(2).map(Number).sort().reverse();
   console.log(args[1]);
