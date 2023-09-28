99c7a880a7e3a495d3b15331c2337b16fcc4c052
Sanae <sanaassim1@gmail.com>
2023-09-28 10:38:14 -0700

    Find a peak in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """A function that finds a peak of a list"""
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
