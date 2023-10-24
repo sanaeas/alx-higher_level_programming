447b31af673b49f4ddd83e6230d69326e9fd548d
sanae <sanaassim1@gmail.com>
2023-10-24 01:56:40 -0700

    Print the title of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+request(url, (error, res, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
