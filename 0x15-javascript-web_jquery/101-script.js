963b27d32d4b19bec896bfa1311f9d2cf9d68cde
sanae <sanaassim1@gmail.com>
2023-11-01 04:03:51 -0700

    List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('<li>Item</li>').appendTo('UL.my_list');
+  });
+
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last-child').remove();
+  });
+
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
+});
